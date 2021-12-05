ea582f551506c88b7ebbcb74173a3e8b3de4b2ef
Metodija747 <metodijabucevski@yahoo.com>
2021-12-05 09:42:47 +0100

    funkcionira dobro

--- models/src/main/resources/sql-scripts/init-Kopj.sql
+++ models/src/main/resources/sql-scripts/init-Kopj.sql
@@ -1,3 +1,4 @@
-INSERT INTO customer (id, date_of_birth, first_name, last_name, address) VALUES (1, TIMESTAMP '2006-01-01 15:36:38', 'James', 'Smith', '32 Threadneedle Street, London');
-INSERT INTO customer (id, date_of_birth, first_name, last_name, address) VALUES (2, TIMESTAMP '2009-03-17 15:36:38', 'John', 'Williams', '21 Foster Lane, London');
-INSERT INTO customer (id, date_of_birth, first_name, last_name, address) VALUES (3, TIMESTAMP '2013-11-29 15:36:38', 'Robert', 'Jones', '11 Ludgate Hill, London');
+INSERT INTO customer (id, date_of_birth, first_name, last_name, address) VALUES (1, TIMESTAMP '1973-08-13 15:36:38', 'Kire', 'Bucevski', 'Kiro Krstevski, Skopje');
+INSERT INTO customer (id, date_of_birth, first_name, last_name, address) VALUES (2, TIMESTAMP '1973-04-28 15:36:38', 'Zaklina', 'Bucevska', 'Metodija Satorov Sarlo, Skopje');
+INSERT INTO customer (id, date_of_birth, first_name, last_name, address) VALUES (3, TIMESTAMP '2004-04-01 15:36:38', 'Mila', 'Bucevska', 'Vladimir Komarov, Skopje');
+INSERT INTO customer (id, date_of_birth, first_name, last_name, address) VALUES (3, TIMESTAMP '1997-11-05 15:36:38', 'Metodija', 'Bucevski', 'Tabor 9, Ljubljana');
