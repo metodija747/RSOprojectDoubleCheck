734bae91dfa6d489e1de8eaf7c7cb2c6075d6517
Metodija747 <metodijabucevski@yahoo.com>
2022-01-05 22:50:35 +0100

    Dobro

--- app/src/main/java/si/fri/rso/samples/Kopj/api/v1/KopjApplication.java
+++ app/src/main/java/si/fri/rso/samples/Kopj/api/v1/KopjApplication.java
@@ -1,7 +1,15 @@
 package si.fri.rso.samples.Kopj.api.v1;
 
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.servers.Server;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+
+@OpenAPIDefinition(info = @Info(title = "Customer details API1", version = "v1", contact = @Contact(email = "mb8696@student.uni-lj.si"), license = @License(name="dev"), description = "API for managing customers."), servers = @Server(url ="http://20.127.141.29/user"))
 @ApplicationPath("/v1")
 public class KopjApplication extends Application{
 
