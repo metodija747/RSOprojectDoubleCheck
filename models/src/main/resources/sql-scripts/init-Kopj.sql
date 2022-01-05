9f68f19239bd7a5058dce62ecbcf5809131ce8cf
Metodija747 <metodijabucevski@yahoo.com>
2022-01-05 22:32:10 +0100

    Dobro

--- models/src/main/resources/sql-scripts/init-Kopj.sql
+++ models/src/main/resources/sql-scripts/init-Kopj.sql
@@ -1,4 +1,4 @@
 INSERT INTO customer (id, date_of_birth, first_name, last_name, address) VALUES (1, TIMESTAMP '1973-08-13 15:36:38', 'Kire', 'Bucevski', 'Kiro Krstevski, Skopje');
 INSERT INTO customer (id, date_of_birth, first_name, last_name, address) VALUES (2, TIMESTAMP '1973-04-28 15:36:38', 'Zaklina', 'Bucevska', 'Metodija Satorov Sarlo, Skopje');
 INSERT INTO customer (id, date_of_birth, first_name, last_name, address) VALUES (3, TIMESTAMP '2004-04-01 15:36:38', 'Mila', 'Bucevska', 'Vladimir Komarov, Skopje');
-INSERT INTO customer (id, date_of_birth, first_name, last_name, address) VALUES (3, TIMESTAMP '1997-11-05 15:36:38', 'Metodija', 'Bucevski', 'Tabor 9, Ljubljana');
+INSERT INTO customer (id, date_of_birth, first_name, last_name, address) VALUES (3, TIMESTAMP '1997-11-05 15:36:38', 'Metodija', 'Bucevski', 'Tabor 9, LjubljanaSLO');
